ad12a06c0d6656b24ecd484b51f7dc8002093362
Julius Knaus <juliusknaus@gmail.com>
2026-08-19 21:30:32 +0200

    commented inits out for the training server

--- design_opt/envs/__init__.py
+++ design_opt/envs/__init__.py
@@ -1,14 +1,14 @@
-from .hopper import HopperEnv
-from .swimmer import SwimmerEnv
-from .ant import AntEnv
-from .gap import GapEnv
-from .walker import WalkerEnv
-from .ant_box import AntPushEnv
-from .walker_box import WalkerPushEnv
-from .swimmer_box import SwimmerPushEnv
-from .ant_box_flip import AntFlipEnv
-from .ant_box_lift import AntLiftEnv
-from .ant_ import AntEnv_
+#from .hopper import HopperEnv
+#from .swimmer import SwimmerEnv
+#from .ant import AntEnv
+#from .gap import GapEnv
+#from .walker import WalkerEnv
+#from .ant_box import AntPushEnv
+#from .walker_box import WalkerPushEnv
+#from .swimmer_box import SwimmerPushEnv
+#from .ant_box_flip import AntFlipEnv
+#from .ant_box_lift import AntLiftEnv
+#from .ant_ import AntEnv_
 from .dex_gripper import DexGripperEnv_
 from .dex_gripper_stacking import DexGripperStackingEnv
 from .dex_gripper_holding import DexGripperHoldingEnv
@@ -18,17 +18,17 @@ from .dex_gripper_adversarial_push import DexGripperAdversarialPushEnv
 from .dex_gripper_multi_push import DexGripperMultiPushEnv
 
 env_dict = {
-    'hopper': HopperEnv,
-    'swimmer': SwimmerEnv,
-    'ant': AntEnv,
-    'gap': GapEnv,
-    'walker': WalkerEnv,
-    'ant_box': AntPushEnv,
-    'walker_box': WalkerPushEnv,
-    'swimmer_box': SwimmerPushEnv,
-    'ant_box_flip': AntFlipEnv,
-    'ant_box_lift': AntLiftEnv,
-    'ant_': AntEnv_, 
+    #'hopper': HopperEnv,
+    #'swimmer': SwimmerEnv,
+    #'ant': AntEnv,
+    #'gap': GapEnv,
+    #'walker': WalkerEnv,
+    #'ant_box': AntPushEnv,
+    #'walker_box': WalkerPushEnv,
+    #'swimmer_box': SwimmerPushEnv,
+    #'ant_box_flip': AntFlipEnv,
+    #'ant_box_lift': AntLiftEnv,
+    #'ant_': AntEnv_, 
     'dex_gripper': DexGripperEnv_,
     'dex_gripper_stacking': DexGripperStackingEnv,
     'dex_gripper_holding': DexGripperHoldingEnv,
